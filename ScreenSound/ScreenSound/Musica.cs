1654cd7fdcf3afec9fb36304f71d8631ef2df597
Edgard <edigarhenriqu@hmail.com>
2023-06-14 15:43:58 -0300

    Orientaçao a Objeto

---
+++ ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Musica
+
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}";
+
+
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Diração: {Duracao}");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponivel no plano.");
+        } else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
